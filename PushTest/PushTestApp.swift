04649979d2235660da35eb1abe634398b9812940
Mike Miller <mike@mikemiller.design>
2025-03-14 09:44:00 -0600

    Initial Commit

---
+++ PushTest/PushTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushTestApp.swift
+//  PushTest
+//
+//  Created by Michael Miller on 3/14/25.
+//
+
+import SwiftUI
+
+@main
+struct PushTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PushTestTests/PushTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  PushTestTests.swift
+//  PushTestTests
+//
+//  Created by Michael Miller on 3/14/25.
+//
+
+import Testing
+@testable import PushTest
+
+struct PushTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
